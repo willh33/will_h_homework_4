e20ffe7cc2110031f95a66bdf8f79d9ab8c0b1f8
willh33 <wharrison33@hotmail.com>
2020-10-08 21:01:41 -0600

    wrote functions to display question and answers

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 let startButton = document.getElementById('start-btn')
 let timerEl = document.getElementById('timer-span');
+let questionTextEl = document.getElementById('question-text');
+let answerOptionsDiv = document.getElementById('multiple-choices-div');
 let totalSeconds = 75;
 let secondsElapsed = 0;
+let currentQuestion = 0;
 
-let questions = [
-	'Commonly used Data Types DO NOT include:', 
-	'The condition in an if / else statement is enclosed within __________:',
-	'Arrays in JavaScript can be used to store _________',
-	'String values must be enclosed within ______ when being assigned to variables',
-	'A very useful tool used during development and debugging for printing content to the debugger is:'
-];
-let answers = [
-	['strings', 'booleans', 'alerts', 'numbers'],
-	['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
-	['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-	['commas', 'curly brackets', 'quotes', 'parenthesis'],
-	['JavaScript', 'terminal / bash', 'for loops', 'console log']
-];
+let questionsAndAnswers = [
+	{
+		question: 'Commonly used Data Types DO NOT include:',
+		answers: ['strings', 'booleans', 'alerts', 'numbers'],
+		correctAnswer: 2
+	},
+	{
+		question: 'The condition in an if / else statement is enclosed within __________:',
+		answers: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
+		correctAnswer: 2
+	},
+	{
+		question: 'Arrays in JavaScript can be used to store _________',
+		answers: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+		correctAnswer: 3
+	},
+	{
+		question: 'String values must be enclosed within ______ when being assigned to variables',
+		answers: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+		correctAnswer: 2
+	},
+	{
+		question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+		answers: ['JavaScript', 'terminal / bash', 'for loops', 'console log'],
+		correctAnswer: 3
+	}
+]
 
 /**
  * Starts the quiz, called on click of the start button
  */
 function startQuiz() {
 	//Start timer
-	interval = setInterval(tick, 1000);
-	
-	
+	interval = setInterval(tick, 1000);	
+	setQuestionAndAnswerText();
+	questionTextEl.classList.remove('text-center');
 }
 
 /**
@@ -40,6 +56,33 @@ function tick() {
 	timerEl.textContent = totalSecondsLeft;
 }
 
+/**
+ * Get the current question and set the question and answer text
+ */
+function setQuestionAndAnswerText() {
+	//Set the question text to current question
+	let question = questionsAndAnswers[currentQuestion];
+	questionTextEl.textContent = question.question;
+
+	answerOptionsDiv.children[0].style.display = 'none';
+	//append a button to the page for each answer
+	for(let i = 0; i < question.answers.length; i++)
+	{
+		console.log(question.answers[i]);
+		let btnDiv = document.createElement('div');
+		btnDiv.classList.add('row')
+
+		let btn = document.createElement('button');
+		btn.classList.add('bg-purple');
+		btn.classList.add('text-white');
+		btn.textContent = (i + 1) + '. ' + question.answers[i];
+
+		btnDiv.append(btn);
+		answerOptionsDiv.append(btnDiv);
+	}
+	startButton.style.display = 'none';
+}
+
 
 /**
  * Check the answer that was clicked, if it was right go to the next question,
@@ -51,4 +94,4 @@ function checkAnswer(event) {
 }
 
 startButton.addEventListener("click", startQuiz);
-answers.addEventListener("click", checkAnswer);
+// answers.addEventListener("click", checkAnswer);
